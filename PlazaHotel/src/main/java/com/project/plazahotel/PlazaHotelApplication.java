34bd106c7f8ca938543cc34ef58b72bd15bbcf93
Akila Ishan Meegoda <it22640116@my.sliit.lk>
2025-02-03 15:42:58 +0530

    initial commit

---
+++ PlazaHotel/src/main/java/com/project/plazahotel/PlazaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.project.plazahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlazaHotelApplication.class, args);
+    }
+
+}
---
+++ PlazaHotel/src/test/java/com/project/plazahotel/PlazaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.plazahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazaHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
